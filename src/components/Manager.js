a9929f3676c4fbebe768d8b8ca417d9d08b446c8
Blue-Brain <89130446037@yandex.ru>
2020-05-31 17:02:36 +0300

    edit forms for Teacher and Manager

--- src/components/Manager.js
+++ src/components/Manager.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import { withFirebase } from './Firebase';
 
 const Manager = () => {
     return  (
         <div>
-            <h1>MANAGER</h1>
+            <h1 className="p-2">Менеджер</h1>
+            <input className="mx-5 my-1" placeholder="Имя" type="text" />
+            <br />
+            <input className="mx-5 my-1" placeholder="Фамилия" type="text" />
+            <br />
+            <input className="mx-5 my-1" type="number" alt="количество занятий" />
+            <br />
+            <button className="btn btn-dark ml-5">Пополнить баланс</button>
         </div>
     )
 }
 
-export default Manager;
+export default withFirebase(Manager);
--- src/components/Teacher.js
+++ src/components/Teacher.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
+import { withFirebase } from "./Firebase";
 
 const Teacher = () => {
-    const [nameTeacher, setNameTeacher] = useState('');
-    const [lastNameTeacher, setLastNameTeacher] = useState('');
+  const [nameTeacher, setNameTeacher] = useState("");
+  const [lastNameTeacher, setLastNameTeacher] = useState("");
 
-    return  (
-        <div>
-            <h1 className="p-2">Учитель</h1>
-            <label className="mx-5"><input className="mr-3"/>Имя</label><br/>
-            <label className="mx-5"><input className="mr-3"/>Фамилия</label><br/>
-            <button className="btn btn-dark ml-5">Показать</button>
-        </div>
-    )
-}
-export default Teacher;
+  return (
+    <div>
+        <h1 className="p-2">Учитель</h1>
+        <input className="mx-5 my-1" placeholder="Имя" type="text"/>
+        <br />
+        <input className="mx-5 my-1" placeholder="Фамилия" type="text"/>
+        <br />
+        <textarea className="mx-5 my-1" placeholder="  Ваш комментарий"></textarea>
+        <br />
+        <input className="mx-5 my-1" type="date"/>
+        <br />
+        <button className="btn btn-dark ml-5">Записать урок</button>
+    </div>
+  );
+};
+export default withFirebase(Teacher);
